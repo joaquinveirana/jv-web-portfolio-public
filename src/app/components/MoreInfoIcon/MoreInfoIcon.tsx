afebd75b2ff8586749a1783bf4f70f4c8b6e9fb9
joaquinveirana <joaquin.veirana@gmail.com>
2023-10-07 03:04:30 -0300

    fix(Header): :bug: Agrando boton de mas informacion en version mobile

--- src/app/components/MoreInfoIcon/MoreInfoIcon.tsx
+++ src/app/components/MoreInfoIcon/MoreInfoIcon.tsx
@@ -10,7 +10,7 @@ export const MoreInfoIcon = (props: IconProps) => {
     <Grow in={true} timeout={props.growTimeout}>
       <div>
         <InfoIcon
-          className='md:w-[24px] md:h-[24px] w-[32px] h-[32px] icon-effect-class'
+          className='md:w-[24px] md:h-[24px] w-[38px] h-[38px] icon-effect-class'
           onClick={() => props.callback()}
         ></InfoIcon>
       </div>
